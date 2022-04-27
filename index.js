e4a5d34027f66f963715c5374dae32b8d3ecc725
Isabela <isabela_cmoreira@hotmail.com>
2022-04-27 17:18:59 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+app.post("/participants", (req, res) => {
+    
+});
+
+app.get("/participants", (req, res) => {
+
+});
+
+app.post("/messages", (req, res) => {
+
+});
+
+app.get("/messages", (req, res) => {
+
+});
+
+
+app.post("/status", (req, res) => {
+
+});
+
+app.listen(5000);
